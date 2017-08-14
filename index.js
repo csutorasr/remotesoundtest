8a061517d6bc22ce0b7505f20cfde5a613ea6c1d
Csutorás Robin <csutorasr@hotmail.com>
2017-08-14 18:17:47 +0200

    Tone works

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('static'));
+app.use(express.static('node_modules/tone/build'));
 
 app.listen(8080, () => {
     console.log('App is listening on 8080');
